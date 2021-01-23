6a74a7a7fe71ca62cf73c85dfccc0d06c1894907
Muhd <muhdsaifulnizam19982gmail.com>
2021-01-23 23:42:11 +0800

    feat: 🎸 Import Setting Models

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 use UploadAble;
+use App\Models\Setting;
 
 class SettingController extends BaseController
 {
--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-// use Config;
+use Config;
+use App\Models\Setting;
 
 class SettingServiceProvider extends ServiceProvider
 {
