33036db3117e1157aee94d83f06577e83b5c0f79
Muhd <muhdsaifulnizam1998@gmail.com>
2021-01-28 02:58:00 +0800

    feat: 🎸 Added delete and update view

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -47,8 +47,8 @@ class CategoryController extends BaseController
             return DataTables::of($data)
                     ->addIndexColumn()
                     ->addColumn('action', function($data){
-                        $btn = '<a href="#" class="btn btn-outline-primary m-1"><i class="fa fa-edit"></i></a>';
-                        $btn .=  '<a href="#" class="btn btn-outline-danger m-1"><i class="fa fa-trash"></i></a>';
+                        $btn = '<a class="btn btn-outline-primary m-1" href="'. route("admin.categories.edit", $data->id) .'"><i class="fa fa-edit"></i></a>';
+                        $btn .=  '<a href="'. route("admin.categories.delete", $data->id) .'" class="btn btn-outline-danger m-1"><i class="fa fa-trash"></i></a>';
                         return $btn;
                     })
                     ->addColumn('parent_name', function($data){
@@ -93,5 +93,55 @@ class CategoryController extends BaseController
             return $this->responseRedirectBack('Error occurred while creating category.', 'error', true, true);
         }
         return $this->responseRedirect('admin.categories.index', 'Category added successfully' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $targetCategory = $this->categoryRepository->findCategoryById($id);
+        $categories = $this->categoryRepository->listCategories();
+
+        $this->setPageTitle('Categories', 'Edit Category : '.$targetCategory->name);
+        return view('admin.categories.edit', compact('categories', 'targetCategory'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'name'      =>  'required|max:191',
+            'parent_id' =>  'required|not_in:0',
+            'image'     =>  'sometimes|mimes:jpg,jpeg,png|max:1000'
+        ]);
+
+        $params = $request->except('_token');
+
+        $category = $this->categoryRepository->updateCategory($params);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while updating category.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Category updated successfully' ,'success',false, false);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function delete($id)
+    {
+        $category = $this->categoryRepository->deleteCategory($id);
+
+        if (!$category) {
+            return $this->responseRedirectBack('Error occurred while deleting category.', 'error', true, true);
+        }
+        return $this->responseRedirect('admin.categories.index', 'Category deleted successfully' ,'success',false, false);
     }
 }
