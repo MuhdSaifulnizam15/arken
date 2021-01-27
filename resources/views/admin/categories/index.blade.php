2508e7535775b5c3d2395d4742382f0bfeb14e0e
Muhd <muhdsaifulnizam1998@gmail.com>
2021-01-28 01:46:05 +0800

    feat: 🎸 added create category button

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -14,8 +14,9 @@
     <div class="row">
       <div class="col-12">
         <div class="card">
-          <div class="card-header">
+          <div class="card-header justify-content-between">
             <h4>{{ $subTitle }}</h4>
+            <a href="{{ route('admin.categories.create') }}" class="btn btn-outline-primary">Create Category</a>
           </div>
           <div class="card-body">
             <div class="table-responsive">
