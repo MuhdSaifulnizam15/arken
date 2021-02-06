799042279654cefa6b45c58d31e0875b8b77fb56
Muhd <muhdsaifulnizam1998@gmail.com>
2021-02-07 04:20:36 +0800

    refactor: refactor additional js of image upload

--- resources/views/admin/products/includes/images.blade.php
+++ resources/views/admin/products/includes/images.blade.php
@@ -8,10 +8,10 @@
             {{ csrf_field() }}
         </form>
     </div>
-    <div class="card-footer bg-whitesmoke text-md-right">
+    <!-- <div class="card-footer bg-whitesmoke text-md-right">
         <button class="btn btn-primary" type="button" id="uploadButton">Upload Images</button>
         <a class="btn btn-outline-secondary" href="{{ route('admin.products.index') }}"><i class="fas fa-trash mr-2"></i>Cancel</a>
-    </div>
+    </div> -->
     @if ($product->images)
         <hr>
         <div class="row">
@@ -30,3 +30,89 @@
         </div>
     @endif
 </div>
+
+@section('additional_js')
+<script type="text/javascript">
+    $('#categories').select2();
+    Dropzone.options.dropzone =
+        {
+            maxFilesize: 10,
+            renameFile: function (file) {
+                var dt = new Date();
+                var time = dt.getTime();
+                return time + file.name;
+            },
+            acceptedFiles: ".jpeg,.jpg,.png,.gif",
+            addRemoveLinks: true,
+            timeout: 60000,
+            success: function (file, response) {
+                console.log(response);
+                swal({  
+                    icon: response.status,
+                    title: response.message,
+                    timer: 3000,
+                })
+                .then((value) => {
+                    switch (value) {
+                        default:
+                        window.location.reload();
+                    }
+                });
+            },
+            error: function (file, response) {
+                return false;
+            }
+        };
+
+    
+        // Delete Image
+    function deleteImage(id){    
+        $(document).ready(function() {
+            $.ajax({
+                url: "/admin/products/delete-image",
+                type: "POST",
+                data:{
+                    _token:'{{ csrf_token() }}',
+                    id: id
+                },
+                cache: false,
+                dataType: 'json',
+                success: function(dataResult){
+                    console.log(dataResult);
+                    swal({  
+                        icon: dataResult.status,
+                        title: dataResult.message,
+                        timer: 3000,
+                        buttons: {
+                            ok: "Ok",
+                        },
+                    })
+                    .then((value) => {
+                        switch (value) {
+                            default:
+                            window.location.reload();
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    // On Click Delete Image Button
+    $(document).on("click", ".delete-image", function() {
+        var id = $(this).attr("id");
+        swal({  
+            icon: "warning",
+            title: "Are you sure you want to delete this image. Images would no longer recoverable later.",
+            buttons: true,
+            dangerMode: true,
+        })
+        .then((willDelete) => {
+            if (willDelete) {
+                console.log(id);
+                deleteImage(id);
+            }
+        });
+    });
+</script>
+@endsection
